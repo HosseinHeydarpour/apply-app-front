a53d9238c13a51540eefad7efe0f49a63b08efe9
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-02-07 11:24:11 +0330

    feat: add auth routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/login/login';
+import { SignUp } from './features/auth/sign-up/sign-up';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'signup',
+    component: SignUp,
+  },
+];
