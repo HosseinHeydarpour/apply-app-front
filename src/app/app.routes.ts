ee5180c6df30218a52c78fcb133fe6c4c41002c4
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-02-07 13:03:03 +0330

    feat: add profile route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/login/login';
 import { SignUp } from './features/auth/sign-up/sign-up';
+import { Profile } from './features/profile/profile';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'signup',
     component: SignUp,
+  },
+  {
+    path: 'profile',
+    component: Profile,
   },
 ];
