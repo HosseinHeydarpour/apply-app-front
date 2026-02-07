d36a120890d7cb7b8e5013dbc7d9fb488958331b
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-02-07 12:12:59 +0330

    feat: complete signup form UI

--- src/app/features/auth/sign-up/sign-up.ts
+++ src/app/features/auth/sign-up/sign-up.ts
@@ -1,11 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { TuiTextfield } from '@taiga-ui/core';
-import { TuiTextarea } from '@taiga-ui/kit';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { TUI_IS_IOS } from '@taiga-ui/cdk';
+import { TuiButton, TuiTextfield } from '@taiga-ui/core';
+import { TuiInputPhone, TuiTextarea } from '@taiga-ui/kit';
 @Component({
   selector: 'app-sign-up',
-  imports: [CommonModule, ReactiveFormsModule, TuiTextarea, TuiTextfield],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    TuiTextarea,
+    TuiTextfield,
+    FormsModule,
+    TuiInputPhone,
+    TuiButton,
+  ],
   templateUrl: './sign-up.html',
   styleUrl: './sign-up.scss',
   host: {
@@ -13,11 +29,26 @@ import { TuiTextarea } from '@taiga-ui/kit';
   },
 })
 export class SignUp {
-  registerForm = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
-    confirmPassword: new FormControl('', [Validators.required]),
-  });
+  registerForm = new FormGroup(
+    {
+      email: new FormControl('', [Validators.required, Validators.email]),
+      // Add phone control here with default +98
+      phone: new FormControl('', [Validators.required, Validators.minLength(12)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required]),
+    },
+    { validators: this.matchPasswords },
+  );
+
+  protected readonly isIos = inject(TUI_IS_IOS);
+
+  // Change default value to Iran code
+  public value = '+98';
+
+  protected get pattern(): string | null {
+    // IOS pattern: allows +98 and spaces/dashes
+    return this.isIos ? '+98[- ]?[0-9-]{1,20}' : null;
+  }
 
   submit() {
     if (this.registerForm.valid) {
@@ -25,5 +56,17 @@ export class SignUp {
     } else {
       this.registerForm.markAllAsTouched();
     }
+  }
+
+  // Custom Validator Function
+  matchPasswords(group: AbstractControl): { [key: string]: boolean } | null {
+    const password = group.get('password')?.value;
+    const confirm = group.get('confirmPassword')?.value;
+    return password === confirm ? null : { notMatching: true };
+  }
+
+  // Helper for HTML to access controls easily
+  get f() {
+    return this.registerForm.controls;
   }
 }
