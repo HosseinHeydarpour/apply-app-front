986551ba3e0560df01e0cf69137dadf1df203a87
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-02-07 13:14:17 +0330

    feat: create navbar(WIP)

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { TuiIcon, TuiIconPipe } from '@taiga-ui/core';
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [TuiIcon, TuiIconPipe, RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar {
-
-}
+export class Navbar {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         'color-bg': '#efede6',
         'color-dark': '#1d1a15',
         'color-gray': '#a4a6b4',
+        'color-orange': '#fe8f72',
         'color-bright': '#b6b6b4',
       },
       fontFamily: {
