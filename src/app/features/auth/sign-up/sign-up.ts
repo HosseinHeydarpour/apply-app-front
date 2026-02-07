78fa299568ed7371bbd0394c19dea1bd373a6292
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-02-07 11:51:01 +0330

    feat(WIP): create sign up form

--- src/app/features/auth/sign-up/sign-up.ts
+++ src/app/features/auth/sign-up/sign-up.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TuiTextfield } from '@taiga-ui/core';
+import { TuiTextarea } from '@taiga-ui/kit';
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, TuiTextarea, TuiTextfield],
   templateUrl: './sign-up.html',
   styleUrl: './sign-up.scss',
+  host: {
+    class: 'pt-8 block',
+  },
 })
 export class SignUp {
+  registerForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    confirmPassword: new FormControl('', [Validators.required]),
+  });
 
+  submit() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+    } else {
+      this.registerForm.markAllAsTouched();
+    }
+  }
 }
