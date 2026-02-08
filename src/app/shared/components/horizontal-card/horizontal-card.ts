069de565fb0595a50d9a285c6b15b1e60d974dfe
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-02-08 09:11:23 +0330

    feat: create card components

---
+++ src/app/shared/components/horizontal-card/horizontal-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-horizontal-card',
+  imports: [],
+  templateUrl: './horizontal-card.html',
+  styleUrl: './horizontal-card.scss',
+})
+export class HorizontalCard {
+
+}
---
+++ src/app/shared/components/vertical-card/vertical-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vertical-card',
+  imports: [],
+  templateUrl: './vertical-card.html',
+  styleUrl: './vertical-card.scss',
+})
+export class VerticalCard {
+
+}
