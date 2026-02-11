42935844a77fc9445a7dadf7b0484cbbeb32700e
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-02-11 08:55:20 +0330

    feat: improve isLoggedin method

--- src/app/core/services/auth.ts
+++ src/app/core/services/auth.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
 import { tap } from 'rxjs';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root',
@@ -32,7 +33,12 @@ export class Auth {
   isLoggedIn(): boolean {
     const token = this.getToken();
     if (!token) return false;
-
-    return true;
+    try {
+      const decoded: any = jwtDecode(token);
+      const isExpired = decoded.exp * 1000 < Date.now();
+      return !isExpired;
+    } catch (error) {
+      return false;
+    }
   }
 }
